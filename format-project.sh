dbdd83bbe569d9f150b2795fb368bf7ca1d13527
Ryan Pavlik <ryan.pavlik@collabora.com>
2019-06-07 21:48:00 -0500

    Be sure to format tests and generated files

--- format-project.sh
+++ format-project.sh
@@ -2,9 +2,8 @@
 # Copyright 2019, Collabora, Ltd.
 # SPDX-License-Identifier: BSL-1.0
 (
-    dirs="include"
     cd $(dirname $0) && \
-    find $dirs \
+    find include tests generated \
       \( -name "*.cpp" -o -name "*.h" \) \
       -exec clang-format -i -style=file \{\} +
 )
